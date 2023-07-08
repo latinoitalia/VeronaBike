73162f805f6e96d15b97c365ee2d8c921be87490
daniele.paoloni <daniele.paoloni@dedagroup.it>
2023-07-08 09:16:36 +0200

    chache

--- src/servicebak.js
+++ src/servicebak.js
@@ -96,30 +96,4 @@ self.addEventListener("fetch", (event) => {
         })
     );
   }
-
-  const cacheName = "tunderforest";
-  if (event.request.destination === "image") {
-    // Open the cache
-    event.respondWith(
-      caches.open(cacheName).then((cache) => {
-        // Respond with the image from the cache or from the network
-        return cache.match(event.request).then((cachedResponse) => {
-          return (
-            cachedResponse ||
-            fetch(event.request.url).then((fetchedResponse) => {
-              // Add the network response to the cache for future visits.
-              // Note: we need to make a copy of the response to save it in
-              // the cache and use the original as the request response.
-              cache.put(event.request, fetchedResponse.clone());
-
-              // Return the network response
-              return fetchedResponse;
-            })
-          );
-        });
-      })
-    );
-  } else {
-    return;
-  }
 });
